f4a5d98cb92ae127c37779e51cbd92c9a5891b23
NieznanyJ <101598645+NieznanyJ@users.noreply.github.com>
2023-12-06 17:37:16 +0100

    Delete src directory

--- src/componets/icons/IconCheck.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function IconCheck() {
-  return (
-    <svg width="10" height="8" xmlns="http://www.w3.org/2000/svg"><path stroke="#fff" stroke-width="2" fill="none" d="m1.276 3.066 2.756 2.756 5-5"/></svg>
-  )
-}
-
-export default IconCheck
